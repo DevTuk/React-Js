b002ef0beca91839eeedb86b2e63c8219b925071
Diego Barros <dienqn84@gmail.com>
2022-05-29 14:36:07 -0300

    elimino comentarios

--- src/Components/ItemsDetail/ItemDetail.js
+++ src/Components/ItemsDetail/ItemDetail.js
@@ -1,15 +1,3 @@
-// import {
-//   Text,
-//   Box,
-//   Image,
-//   AspectRatio,
-//   Skeleton,
-//   useBreakpointValue,
-// } from '@chakra-ui/react';
-// import ItemCount from '../ItemCount/ItemCount';
-
-// const ItemDetail = (props) => {
-//   return (
 import {
   Container,
   Stack,
@@ -132,40 +120,3 @@ export const Blob = (props) => {
 };
 
 export default ItemDetail;
-
-// <Box
-//   bg='#ffffff'
-//   width='sm'
-//   minHeight='sm'
-//   overflow='hidden'
-//   // color='blackAlpha.500'
-//   textAlign='center'
-//   bgGradient='linear(to-b, green.300, white)'
-//   fontWeight='bold'
-//   rounded={50}
-// >
-//   <AspectRatio ratio={4 / 5}>
-//     <Image
-//       src={props.imagen}
-//       alt={props.nombre}
-//       draggable='false'
-//       fallback={<Skeleton />}
-//       borderRadius={useBreakpointValue({ base: 'md', md: 'xl' })}
-//       roundedBottom={40}
-//     />
-//   </AspectRatio>
-
-//   <Box p={4} pb={6} textAlign='center' color='gray.800'>
-//     <Text fontWeight='bold' fontSize='xl' color='gray.800'>
-//       {props.nombre}
-//     </Text>
-//     <Text fontSize='sm' mb={3} color='gray.800'>
-//       {props.precio}
-//     </Text>
-//     <Text fontSize='sm' mb={3} color='gray.800'>
-//       stock: {props.stock}
-//     </Text>
-//   </Box>
-//   <ItemCount stock={props.stock} />
-// </Box>
-// //card de detalles
