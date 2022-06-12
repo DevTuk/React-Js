9ebbdf89da558e0530c0256b488b26de9752d380
Diego Barros <dienqn84@gmail.com>
2022-06-12 20:19:16 -0300

    vuelo a subir código

--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom';
 import { Loader } from '../Loader/Loader';
 import { getDocs, collection, query, where } from 'firebase/firestore';
 import { db } from '../../Services/Firebase';
+import { Box, Text } from '@chakra-ui/react';
 
 const ItemListContainer = () => {
   const [products, setProductos] = useState([]);
@@ -61,7 +62,28 @@ const ItemListContainer = () => {
 
   return (
     <>
-      <ItemList producto={products} />
+      {products.length > 0 ? (
+        <ItemList producto={products} />
+      ) : (
+        <Box
+          alignItems='center'
+          border='1px solid #c5caff'
+          mx={10}
+          pl={3}
+          pr={3}
+          pt={5}
+          pb={5}
+          rounded={10}
+        >
+          <Text
+            fontWeight={700}
+            textAlign='center'
+            fontSize={{ base: '2xl', sm: '2xl' }}
+          >
+            Ups... ¡No hay productos!
+          </Text>
+        </Box>
+      )}
     </>
   );
 };
